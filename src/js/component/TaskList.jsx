9378051b3f2ab5a0ef230ab08df221a77d1946f9
nanher94 <hernan.albiol@gmail.com>
2022-10-23 19:39:27 +0000

    To-Do List with ReactJS and Fetch

--- src/js/component/TaskList.jsx
+++ src/js/component/TaskList.jsx
@@ -1,11 +1,64 @@
-import React, { useState, MouseEvent } from "react";
+import React, { useState, MouseEvent, useEffect } from "react";
 
 function TaskList() {
   const [newTask, setNewTask] = useState("");
   const [taskList, setTaskList] = useState([]);
 
+  useEffect(() => {
+    // createNewUser();
+  }, []);
+
+  function createNewUser() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/hernan", {
+      method: "POST",
+      body: JSON.stringify([{ label: "hola", done: false }]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp; //(returns promise) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+      })
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
+  }
+
+  function sendTodoList(toDos) {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
+      method: "PUT",
+      body: JSON.stringify(toDos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp; //(returns promise) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+      })
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
+  }
+
   const addTask = (e) => {
     e.preventDefault();
+
     if (!newTask.trim()) {
       alert("ERROR: Tarea en blanco");
       setNewTask("");
@@ -14,10 +67,14 @@ function TaskList() {
 
     const task = {
       id: Math.floor(Math.random() * 1000),
-      value: newTask,
+      label: newTask,
+      done: false,
     };
 
     setTaskList((oldTaskList) => [...oldTaskList, task]);
+
+    sendTodoList(taskList);
+
     setNewTask("");
   };
 
@@ -26,15 +83,17 @@ function TaskList() {
     setTaskList(newTaskList);
   };
 
+  // NO SE PORQUE EL PRIMERO ME DA ERROR Y DESPUES SI FUNCIONA
+
   return (
     <div
       className="container border mt-3 text-center"
       style={{ width: "400px" }}
     >
-      <h1>ToDo's ✍🏼</h1>
+      <h1>To-Do List ✍🏼</h1>
       <form className="d-flex justify-content-center p-3" onSubmit={addTask}>
         <input
-          className="border-0 border-bottom"
+          className="border-0 border-bottom p-2"
           type="text"
           value={newTask}
           placeholder="Agrega una tarea"
@@ -55,10 +114,10 @@ function TaskList() {
           : taskList.map((tarea) => {
               return (
                 <li
-                  className="d-flex justify-content-start align-items-center border-top border-bottom"
+                  className="d-flex justify-content-start align-items-center border-bottom"
                   key={tarea.id}
                 >
-                  {tarea.value}
+                  {tarea.label}
                   <button
                     className="btn btn-danger btn-sm ms-auto"
                     onClick={() => deleteToDo(tarea.id)}
